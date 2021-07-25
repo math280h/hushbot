9427bc40c2ac40db147905e08470b7c0e947e20e
Mathias V. Nielsen <hube321@gmail.com>
2021-07-25 03:01:47 +0200

    Moved redis, filter, helpers, and commands into the Hush class

--- src/bot.py
+++ src/bot.py
@@ -8,19 +8,6 @@ from src.modules.messenger import Messenger
 import discord
 import redis
 
-# Storage
-message_filter_storage = redis.Redis(
-    host=getenv("REDIS_HOST"),
-    port=getenv("REDIS_PORT"),
-    username=getenv("REDIS_USER"),
-    password=getenv("REDIS_PASS")
-)
-
-
-msg_filter = Filter(message_filter_storage)
-helpers = Helpers()
-commands = Commands(helpers)
-
 
 class Hush(discord.Client):
     def __init__(self, **options):
@@ -28,35 +15,47 @@ class Hush(discord.Client):
         super().__init__(**options)
         self.messenger = None
 
+        # Storage
+        self.message_filter_storage = redis.Redis(
+            host=getenv("REDIS_HOST"),
+            port=getenv("REDIS_PORT"),
+            username=getenv("REDIS_USER"),
+            password=getenv("REDIS_PASS")
+        )
+
+        self.msg_filter = Filter(self.message_filter_storage)
+        self.helpers = Helpers()
+        self.commands = Commands(self.helpers)
+
     async def on_ready(self):
         print('Connected!')
         print('Username: {0.name}\nID: {0.id}'.format(self.user))
         self.messenger = Messenger(
-            self.get_channel(helpers.config["channels"]["log_channel"]),
-            self.get_channel(helpers.config["channels"]["alert_channel"])
+            self.get_channel(self.helpers.config["channels"]["log_channel"]),
+            self.get_channel(self.helpers.config["channels"]["alert_channel"])
         )
 
     async def on_message(self, message: discord.Message):
-        if await helpers.is_command(
+        if await self.helpers.is_command(
                 message,
                 discord.utils.get(
                     message.guild.roles,
-                    id=helpers.config["permissions"]["staff_role"]
+                    id=self.helpers.config["permissions"]["staff_role"]
                 )
         ):
-            command, args = await helpers.get_command(message)
+            command, args = await self.helpers.get_command(message)
 
             cmd_list = {
-                "help": commands.man,
-                "blacklist": commands.bl
+                "help": self.commands.man,
+                "blacklist": self.commands.bl
             }
 
-            cmd = cmd_list.get(command, commands.man)
-            await cmd(message, args, message_filter_storage)
+            cmd = cmd_list.get(command, self.commands.man)
+            await cmd(message, args, self.message_filter_storage)
 
         elif not message.author.bot:
             # Filter messages
-            result, word = await msg_filter.filter(message.content)
+            result, word = await self.msg_filter.filter(message.content)
 
             if result is None:
                 # Nothing wrong detected with this message
